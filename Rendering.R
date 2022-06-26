136a9eb6ffd423d87df8d249e930e61f801e265f
mclightf <mclightf@ncsu.edu>
2022-06-26 19:17:30 -0400

    Fixing R file

--- Rendering.R
+++ Rendering.R
@@ -1,7 +1,7 @@
 #Rendering Code
 rmarkdown::render("_Rmd/Pokemon-API.Rmd", 
                   output_format = "github_document",
-                  output_file = "README.md",
+                  output_file = "../README.md",
                   output_dir = "README_files",
                   output_options = list(
                     df_print = "default",
